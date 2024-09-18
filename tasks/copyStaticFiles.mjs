b676008ff533c977a518ef23c0bc5a44f5323442
steamdroid <steamdroid77@gmail.com>
2024-09-18 15:40:33 +0300

    fix copyStaticFiles task

--- tasks/copyStaticFiles.mjs
+++ tasks/copyStaticFiles.mjs
@@ -1,19 +1,20 @@
 import gulp from 'gulp';
-const {
-	src,
-	dest
-} = gulp;
 
 import changedInPlace from 'gulp-changed-in-place';
 
 import dotenv from 'dotenv';
+
+const {
+  src,
+  dest,
+} = gulp;
 dotenv.config();
 
-export default function copyStaticFiles() { return src([
-  `${process.env.SRC_DIR}/video/**/*.*`,
-  `${process.env.SRC_DIR}/img/**/*.{svg,gif}`,
-  `${process.env.SRC_DIR}/manifest.webmanifest`,
-], { base: `${process.env.SRC_DIR}/` })
-  .pipe(changedInPlace({ firstPass: true, howToDetermineDifference: 'hash' }))
-  .pipe(dest(process.env.DEST_DIR));
+export default function copyStaticFiles() {
+  return src([
+    `${process.env.SRC_DIR}/**/*.html`,
+    `${process.env.SRC_DIR}/img/**/*.{svg,gif}`,
+  ], { base: `${process.env.SRC_DIR}/` })
+    .pipe(changedInPlace({ firstPass: true, howToDetermineDifference: 'hash' }))
+    .pipe(dest(process.env.DEST_DIR));
 }
