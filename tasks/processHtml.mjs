f1805677b03a3459b4f3a01a39b0e8bc66ec9e81
steamdroid <steamdroid77@gmail.com>
2024-09-20 20:35:57 +0300

    fix build assets linking

--- tasks/processHtml.mjs
+++ tasks/processHtml.mjs
@@ -1,5 +1,7 @@
 import gulp from 'gulp';
 
+import gulpIf from 'gulp-if';
+import replace from 'gulp-replace';
 import changedInPlace from 'gulp-changed-in-place';
 import embedSvg from 'gulp-embed-svg';
 
@@ -20,5 +22,7 @@ export default function processHtml() {
       root: './src',
       xmlMode: false,
     }))
+    .pipe(gulpIf(process.env.NODE_ENV === 'production', replace('.js"', `.min.js?v=${Date.now()}"`)))
+    .pipe(gulpIf(process.env.NODE_ENV === 'production', replace('.css"', `.min.css?v=${Date.now()}"`)))
     .pipe(dest(process.env.DEST_DIR));
 }
