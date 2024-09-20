560a9e85caf36ac57c708efb8178540b95d1a4b4
steamdroid <steamdroid77@gmail.com>
2024-09-20 13:07:36 +0300

    fix sass import

--- tasks/compileStyles.mjs
+++ tasks/compileStyles.mjs
@@ -1,16 +1,11 @@
 import gulp from 'gulp';
-const {
-	src,
-	dest,
-} = gulp;
 
 import plumber from 'gulp-plumber';
 import rename from 'gulp-rename';
 import sourcemaps from 'gulp-sourcemaps';
 
-import dartSass from 'sass';
+import * as dartSass from 'sass';
 import gulpSass from 'gulp-sass';
-const sass = gulpSass(dartSass);
 
 import postcss from 'gulp-postcss';
 import atImport from 'postcss-easy-import';
@@ -21,26 +16,33 @@ import sync from 'browser-sync';
 import gulpIf from 'gulp-if';
 
 import dotenv from 'dotenv';
+
+const {
+  src,
+  dest,
+} = gulp;
+const sass = gulpSass(dartSass);
 dotenv.config();
 
-export default function compileStyles() {return src([`${process.env.SRC_DIR}/scss/main.scss`])
-  .pipe(plumber())
-  .pipe(sourcemaps.init())
-  .pipe(sass.sync().on('error', sass.logError))
-  .pipe(postcss([
-    atImport(),
-    autoprefixer(),
-  ]))
-  .pipe(rename((filePath) => {
-    filePath.basename = 'style';
-    filePath.extname = '.css';
-  }))
-  .pipe(dest(`${process.env.DEST_DIR}/css/`))
-  .pipe(gulpIf(process.env.NODE_ENV === 'production', cleanCSS()))
-  .pipe(gulpIf(process.env.NODE_ENV === 'production', rename({
-    suffix: '.min',
-  })))
-  .pipe(sourcemaps.write('./'))
-  .pipe(dest(`${process.env.DEST_DIR}/css/`))
-  .pipe(sync.stream());
+export default function compileStyles() {
+  return src([`${process.env.SRC_DIR}/scss/main.scss`])
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(postcss([
+      atImport(),
+      autoprefixer(),
+    ]))
+    .pipe(rename((filePath) => {
+      filePath.basename = 'style';
+      filePath.extname = '.css';
+    }))
+    .pipe(dest(`${process.env.DEST_DIR}/css/`))
+    .pipe(gulpIf(process.env.NODE_ENV === 'production', cleanCSS()))
+    .pipe(gulpIf(process.env.NODE_ENV === 'production', rename({
+      suffix: '.min',
+    })))
+    .pipe(sourcemaps.write('./'))
+    .pipe(dest(`${process.env.DEST_DIR}/css/`))
+    .pipe(sync.stream());
 }
